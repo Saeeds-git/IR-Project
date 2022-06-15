e55af3f5d0bda32c378d8667dd14d2419d227429
Saeed Asadollahi <Saeed.ssdr@gmail.com>
2022-06-15 22:50:15 +0430

    Final changes applied

--- digikalaMag-scrape.py
+++ digikalaMag-scrape.py
@@ -1,47 +1,40 @@
-
 import requests
 from bs4 import BeautifulSoup
-from newspaper import Article
 from tqdm import tqdm
-import pandas as pd
+from newspaper import Article
+import pandas as panda
 
 
-def article_crawler(year: int):
+def article_crawler():
     page = 58
     scraped_data = []
-    url_list = []
 
     while True:
         page += 1
-
         main_page_url = f"https://www.digikala.com/mag/tag/%d9%86%da%a9%d8%aa%d9%87-%d9%88-%d8%aa%d8%b1%d9%81%d9%86%d8%af/page/{page}/"
-
+        
         html = requests.get(main_page_url).text  # Download html of the page
-
-        soup = BeautifulSoup(html, features='lxml')
-
-        links = soup.find_all('div' , {"class": "masonry-gallery__item"})
+        B_soup = BeautifulSoup(html, features='lxml')
+        links = B_soup.find_all('div' , {"class": "masonry-gallery__item"})
 
         # to get out of the loop when we get to end of posts
         if len(links) == 0 : break
 
         for link in tqdm(links):
-            
-            link2 = link.find_all("a")[0]
-            page_url = link2["href"]
-            url_list.append(page_url)
+            a_tag = link.find_all("a")[0]
+            page_url = a_tag["href"]
             try:
                 article = Article(page_url)
                 article.download()
                 article.parse()
-                scraped_data.append({'url': page_url, 'text': article.text, 'title': article.title})
+                scraped_data.append({'url': page_url, 'title': article.title, 'text': article.text})
             except:
                 print(f"Failed to process page: {page_url}")
 
-    df = pd.DataFrame(scraped_data)
+    df = panda.DataFrame(scraped_data)
     df.to_csv(f'D:\IR\digikalaMagTricks&Points.csv')
 
 
 if __name__ == '__main__':
-    article_crawler(1400)
+    article_crawler()
 
